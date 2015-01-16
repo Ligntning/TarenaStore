e29b5be12cbf5a8bdd646319e246182f3e9016d3
Fred <Fred@wang>
2015-01-16 11:33:12 +0800

    +error界面

---
+++ src/sduwh/dream/web/errorServlet.java
@@ -0,0 +1,38 @@
+package sduwh.dream.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class errorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		String message = (String)request.getAttribute("message");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
+		out.println("<title>无标题文档</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>对不起，您访问的页面出错</p>");
+		out.println("<p> "+ message+"</p>");
+		out.println("</body>");
+		out.println("</html>");
+			
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
